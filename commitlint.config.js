ef3e4ae191f8c9cde76390dddd0c25e3fa8843d0
Edgar Zavala <ezavile@gmail.com>
2020-01-25 23:30:28 -0600

    chore: add commitlint configurations

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const scopes = require('@commitlint/config-lerna-scopes');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': ctx =>
+      scopes.rules['scope-enum'](ctx).then(([level, applicable, packages]) => [
+        level,
+        applicable,
+        packages,
+      ]),
+  },
+};
