4f04817b8c0a9d5f7131492f87b362464861b5d5
Edgar Zavala <ezavile@gmail.com>
2020-01-25 23:00:21 -0600

    chore: add typescript, prettier and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  settings:  {
+    react:  {
+      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  }
+};
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  trailingComma: 'es5',
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  endOfLine: 'lf',
+};
