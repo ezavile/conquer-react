dbd12d8e5824b4f8669c9e2dca6f39bab52b26d4
Edgar Zavala <ezavile@gmail.com>
2020-01-29 00:10:33 -0600

    feat(forecast): integrate unsplash-js to get random photo

--- packages/forecast/src/app.tsx
+++ packages/forecast/src/app.tsx
@@ -1,11 +1,39 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+import Unsplash, { toJson } from 'unsplash-js';
+import dotenv from 'dotenv';
+import { Wallpaper } from './components';
 
-// eslint-disable-next-line import/no-unresolved
 import './app.css';
 
-const App = () => (
-  <div className="bg-blue-100">I am conquering the react world!</div>
-);
+dotenv.config();
+
+const unsplash = new Unsplash({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY as string,
+});
+
+const App = () => {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    // TODO: move logic to state handler
+    unsplash.photos
+      .getRandomPhoto({
+        query: 'mexico',
+        orientation: 'landscape',
+        collections: ['mexico', 'mexico nature', 'mexico outdoor'],
+      })
+      .then(toJson)
+      .then(photo => {
+        setUrl(photo.urls.regular);
+      });
+  }, []);
+
+  return (
+    <div className="bg-blue-100">
+      <Wallpaper url={url} />
+    </div>
+  );
+};
 
 render(<App />, document.getElementById('root'));
