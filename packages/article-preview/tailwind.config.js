d1f16ab2340af676140eccb297d6c1ee4d620da1
Edgar Zavala <ezavile@gmail.com>
2020-08-19 20:02:31 -0500

    feat(article-preview): add custom tailwind values
    - adjust dimensions for responsiveness
    - change font family base

--- packages/article-preview/tailwind.config.js
+++ packages/article-preview/tailwind.config.js
@@ -1,6 +1,12 @@
+const plugin = require('tailwindcss/plugin');
 const base = require('@conquer-react/craco-config/tailwind.config');
 
 // TODO: improve destructing using immerjs
+
+const newDimensions = {
+  '72': '18rem',
+  '80': '20rem',
+};
 
 module.exports = {
   ...base,
@@ -15,6 +21,27 @@ module.exports = {
         'grayish-blue': 'hsl(212, 23%, 69%)',
         'light-grayish-blue': 'hsl(210, 46%, 95%)',
       },
+      height: newDimensions,
+      width: newDimensions,
+      letterSpacing: {
+        widest: '0.4em',
+      },
     },
   },
+  plugins: [
+    plugin(({ addBase }) => {
+      addBase({
+        body: {
+          fontFamily: 'Manrope',
+          fontSize: '13px',
+          lineHeight: 1.6,
+          fontWeight: 500,
+        },
+        h2: {
+          fontSize: '1.2rem',
+          fontWeight: 700,
+        },
+      });
+    }),
+  ],
 };
