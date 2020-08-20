218ad625461c161f23c89b916561f86f57de4216
Edgar Zavala <ezavile@gmail.com>
2020-08-19 20:57:30 -0500

    feat(article-preview): add solution for challenge

---
+++ packages/article-preview/src/components/author/author.spec.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, RenderResult } from '@testing-library/react';
+import { Author, AuthorProps } from './author';
+
+describe('<Author />', () => {
+  let rendered: RenderResult;
+
+  const author: AuthorProps = {
+    avatar: 'some-avatar.png',
+    name: 'Edgar Zavala',
+    publishedAt: '27 June 2020',
+  };
+
+  beforeEach(() => {
+    rendered = render(<Author {...author} />);
+  });
+
+  it('sets given avatar as an image', () => {
+    const img = rendered.container.querySelector('img');
+
+    expect(img).toHaveAttribute('src', author.avatar);
+  });
+
+  it('sets given name as strong tag', () => {
+    const name = rendered.container.querySelector('strong');
+
+    expect(name).toHaveTextContent(author.name);
+  });
+
+  it('sets given published date as time tag', () => {
+    const publishedAt = rendered.container.querySelector('time');
+
+    expect(publishedAt).toHaveTextContent(author.publishedAt);
+  });
+});
---
+++ packages/article-preview/src/components/background-image/background-image.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface BackgroundImageProps {
+  src: string;
+}
+
+export const BackgroundImage: React.FC<BackgroundImageProps> = ({ src }) => (
+  <div
+    className="bg-center bg-cover rounded-t-md h-56 md:h-auto md:w-80 md:bg-left md:rounded-t-none md:rounded-l-md md:flex-shrink-0"
+    style={{ backgroundImage: `url(${src})` }}
+  />
+);
