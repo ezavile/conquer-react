a6effe52ed4385c48e4c23aef3b1c1fdb02eedc2
Edgar Zavala <ezavile@gmail.com>
2020-08-25 03:06:42 -0500

    feat(coding-bootcamp-testimonials-slider): add basic tailwind config

--- packages/coding-bootcamp-testimonials-slider/tailwind.config.js
+++ packages/coding-bootcamp-testimonials-slider/tailwind.config.js
@@ -1,3 +1,27 @@
+const plugin = require('tailwindcss/plugin');
 const base = require('@conquer-react/craco-config/tailwind.config');
 
-module.exports = base;
+module.exports = {
+  ...base,
+  theme: {
+    ...base.theme,
+    extend: {
+      ...base.theme.extend,
+      colors: {
+        ...base.theme.extend.color,
+        'dark-blue': 'hsl(240, 38%, 20%)',
+        'grayish-blue': 'hsl(240, 18%, 77%)',
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addBase }) => {
+      addBase({
+        body: {
+          fontFamily: 'Inter',
+          fontSize: '32px',
+        },
+      });
+    }),
+  ],
+};
