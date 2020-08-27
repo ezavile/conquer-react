a46c67213d58a9d740d08d808df8bc2364e8ef4c
Edgar Zavala <ezavile@gmail.com>
2020-08-27 01:17:17 -0500

    feat(coding-bootcamp-testimonials-slider): add basic markup

--- packages/coding-bootcamp-testimonials-slider/tailwind.config.js
+++ packages/coding-bootcamp-testimonials-slider/tailwind.config.js
@@ -1,6 +1,13 @@
 const plugin = require('tailwindcss/plugin');
 const base = require('@conquer-react/craco-config/tailwind.config');
 
+const newDimensions = {
+  '72': '20rem',
+  '80': '34rem',
+  '84': '41rem',
+  '88': '43.5rem',
+};
+
 module.exports = {
   ...base,
   theme: {
@@ -12,6 +19,14 @@ module.exports = {
         'dark-blue': 'hsl(240, 38%, 20%)',
         'grayish-blue': 'hsl(240, 18%, 77%)',
       },
+      height: newDimensions,
+      width: newDimensions,
+      maxWidth: {
+        '7xl': '78rem',
+      },
+      fontSize: {
+        '4xl': '32px',
+      }
     },
   },
   plugins: [
