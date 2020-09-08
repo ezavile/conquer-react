cc10c3b351d75f1b453519f4b3ac9145a74b4dcc
Edgar Zavala <ezavile@gmail.com>
2020-09-08 02:35:04 -0500

    feat(social-media-dashboard): add react-icons

--- packages/social-media-dashboard/src/App.tsx
+++ packages/social-media-dashboard/src/App.tsx
@@ -1,3 +1,6 @@
+import { GrFacebook, GrTwitter, GrInstagram, GrYoutube } from 'react-icons/gr';
+import { FaCaretUp, FaCaretDown } from 'react-icons/fa';
+
 import React from 'react';
 
 export function App() {
@@ -25,7 +28,9 @@ export function App() {
           <article className="cr_DashboardCard cr_DashboardCard--followers">
             <h3 className="cr_u-srOnly">facebook</h3>
             <div className="cr_DashboardCard-title">
-              <span className="cr_DashboardCard-title-icon">[f]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrFacebook color="#178FF5" size="1.25rem" />
+              </span>
               <span className="cr_DashboardCard-title-subheading cr_u-subheading">
                 @nathanf
               </span>
@@ -38,7 +43,9 @@ export function App() {
                 </span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">12 Today</span>
               </div>
             </div>
@@ -46,7 +53,9 @@ export function App() {
           <article className="cr_DashboardCard cr_DashboardCard--followers">
             <h3 className="cr_u-srOnly">twitter</h3>
             <div className="cr_DashboardCard-title">
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrTwitter color="#1DA1F2" size="1.25rem" />
+              </span>
               <span className="cr_DashboardCard-title-subheading cr_u-subheading">
                 @nathanf
               </span>
@@ -59,7 +68,9 @@ export function App() {
                 </span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">99 Today</span>
               </div>
             </div>
@@ -67,7 +78,9 @@ export function App() {
           <article className="cr_DashboardCard cr_DashboardCard--followers">
             <h3 className="cr_u-srOnly">instagram</h3>
             <div className="cr_DashboardCard-title">
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrInstagram color="#EE877E" size="1.25rem" />
+              </span>
               <span className="cr_DashboardCard-title-subheading cr_u-subheading">
                 @realnathanf
               </span>
@@ -80,7 +93,9 @@ export function App() {
                 </span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">1099 Today</span>
               </div>
             </div>
@@ -88,7 +103,9 @@ export function App() {
           <article className="cr_DashboardCard cr_DashboardCard--followers">
             <h3 className="cr_u-srOnly">youtube</h3>
             <div className="cr_DashboardCard-title">
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrYoutube color="#C4032B" size="1.25rem" />
+              </span>
               <span className="cr_DashboardCard-title-subheading cr_u-subheading">
                 Nathan F.
               </span>
@@ -101,7 +118,9 @@ export function App() {
                 </span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--down">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretDown />
+                </span>
                 <span className="cr_DashboardCard-stats-value">144 Today</span>
               </div>
             </div>
@@ -114,16 +133,20 @@ export function App() {
           <article className="cr_DashboardCard cr_DashboardCard--subscriptions">
             <div className="cr_DashboardCard-title">
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
-                Page Views Facebook
+                Page Views
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrFacebook color="#178FF5" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">87</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">3%</span>
               </div>
             </div>
@@ -133,14 +156,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Likes
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrFacebook color="#178FF5" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">52</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--down">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretDown />
+                </span>
                 <span className="cr_DashboardCard-stats-value">2%</span>
               </div>
             </div>
@@ -150,14 +177,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Likes
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrInstagram color="#EE877E" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">5462</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">2257%</span>
               </div>
             </div>
@@ -167,14 +198,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Profile Views
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrInstagram color="#EE877E" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">52K</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">1375%</span>
               </div>
             </div>
@@ -184,14 +219,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Retweets
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrTwitter color="#1DA1F2" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">117</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">303%</span>
               </div>
             </div>
@@ -201,14 +240,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Likes
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrTwitter color="#1DA1F2" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">507</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--up">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretUp />
+                </span>
                 <span className="cr_DashboardCard-stats-value">553%</span>
               </div>
             </div>
@@ -218,14 +261,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Likes
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrYoutube color="#C4032B" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">107</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--down">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretDown />
+                </span>
                 <span className="cr_DashboardCard-stats-value">19%</span>
               </div>
             </div>
@@ -235,14 +282,18 @@ export function App() {
               <h3 className="cr_DashboardCard-title-label cr_u-subheading">
                 Total Views
               </h3>
-              <span className="cr_DashboardCard-title-icon">[t]</span>
+              <span className="cr_DashboardCard-title-icon">
+                <GrYoutube color="#C4032B" size="1.25rem" />
+              </span>
             </div>
             <div className="cr_DashboardCard-metrics">
               <div className="cr_DashboardCard-subscribers">
                 <span className="cr_DashboardCard-subscribers-value">1407</span>
               </div>
               <div className="cr_DashboardCard-stats cr_DashboardCard-stats--down">
-                <span className="cr_DashboardCard-stats-icon">[^]</span>
+                <span className="cr_DashboardCard-stats-icon">
+                  <FaCaretDown />
+                </span>
                 <span className="cr_DashboardCard-stats-value">12%</span>
               </div>
             </div>
