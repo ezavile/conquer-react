caa68c2bf2d92eb0046d1992b07743592bb3cfaf
Edgar Zavala <ezavile@gmail.com>
2021-02-16 21:28:19 -0600

    refactor(clock-app): move images to its own file

---
+++ packages/clock-app/src/components/background-image/images.tsx
@@ -0,0 +1,19 @@
+import smDay from './sm-daytime.jpg';
+import mdDay from './md-daytime.jpg';
+import lgDay from './lg-daytime.jpg';
+import smNight from './sm-nighttime.jpg';
+import mdNight from './md-nighttime.jpg';
+import lgNight from './lg-nighttime.jpg';
+
+export const IMAGES = {
+  daytime: {
+    sm: smDay,
+    md: mdDay,
+    lg: lgDay,
+  },
+  nighttime: {
+    sm: smNight,
+    md: mdNight,
+    lg: lgNight,
+  },
+};
